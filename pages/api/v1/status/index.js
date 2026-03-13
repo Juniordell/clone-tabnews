5119e9fbd79bb9395055f8aad635d0791702cee8
Nelson Dell <juniordell123@gmail.com>
2026-03-13 15:35:03 +0000

    finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,32 @@
 import database from "infra/database.js";
 
+// Controller
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  response.status(200).json({ msg: "Curso.dev é fora da curva!" });
+  const updatedAt = new Date().toISOString();
+
+  const postgresVersion = await database.query("SHOW server_version;");
+  const postgresVersionValeu = postgresVersion.rows[0].server_version;
+
+  const maxConnections = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = maxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const usedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const usedConnectionsValue = usedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: postgresVersionValeu,
+        max_connections: parseInt(maxConnectionsValue),
+        used_connections: usedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,26 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const updatedAtParsed = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toBe(updatedAtParsed);
+
+  const postgresVersion = responseBody.dependencies.database.version;
+  expect(postgresVersion).toEqual("16.0");
+
+  const maxConnections = responseBody.dependencies.database.max_connections;
+  expect(maxConnections).toEqual(100);
+
+  const usedConnections = responseBody.dependencies.database.used_connections;
+  expect(usedConnections).toEqual(1);
 });
+
+// test.only("GET to /api/v1/status should return 200", async () => {
+//   await fetch("http://localhost:3000/api/v1/status?database_name=local_db");
+//   await fetch(
+//     "http://localhost:3000/api/v1/status?database_name='; SELECT pg_sleep(4); --",
+//   );
+// });
